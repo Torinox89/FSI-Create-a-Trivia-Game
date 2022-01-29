f9cecff21afbace640dce91d74bced6c33504959
unknown <mitrehealthcare@gmail.com>
2022-01-28 22:07:11 -0800

    correct answer apper on dev-tools

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ for(let i = 0; i < questions.length; i++){
 for(let i = 0; i < questions.length; i++){
     let question = questions[i]
     let userAnswer = window.prompt(question.text)
-    
+    if(userAnswer === question.correctAnswer){
+        console.log('Correct')
+    }
 }
