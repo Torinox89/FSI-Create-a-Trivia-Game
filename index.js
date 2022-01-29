a7b537495c24a182debbc28e6da0f6153ed54559
unknown <mitrehealthcare@gmail.com>
2022-01-28 21:36:36 -0800

    add a name, and Q = Q[i}

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your Code Here
+
+let userName = window.prompt('Please enter your name:')
+
+for(let i = 0; i < questions.length; i++){
+    console.log(questions[i])
+}
+
+let userName = window.prompt('Please enter your name:')
+
+for(let i = 0; i < questions.length; i++){
+    let question = questions[i]
+
+}
+
